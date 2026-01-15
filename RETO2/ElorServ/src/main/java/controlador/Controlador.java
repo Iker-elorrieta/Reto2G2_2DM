6168b4c646c1b95b26bdba32e58986d5e45de6cb
AlvaroMorci <morcilloserna2005@gmail.com>
2026-01-15 14:06:59 +0100

    creo metodo login en la api

---
+++ RETO2/ElorServ/src/main/java/controlador/Controlador.java
@@ -0,0 +1,170 @@
+package controlador;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import javax.swing.JOptionPane;
+
+
+import vista.Principal;
+import vista.Principal.enumAcciones;
+
+public class Controlador implements ActionListener, MouseListener {
+
+	private vista.Principal vistaPrincipal;
+	private Socket cliente;
+	private DataOutputStream dos;
+	private DataInputStream dis;
+	private ObjectInputStream ois;
+	private ObjectOutputStream oos;
+	private int id = 0;
+
+	/*
+	 * *** CONSTRUCTORES ***
+	 */
+	public Controlador(vista.Principal vistaPrincipal) {
+		this.vistaPrincipal = vistaPrincipal;
+		this.inicializarControlador();
+	}
+
+	private void inicializarControlador() {
+
+		try {
+			cliente = new Socket("localhost", 4500);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.vistaPrincipal.getPanelLogin().getBtnLogin().addActionListener(this);
+		this.vistaPrincipal.getPanelLogin().getBtnLogin().setActionCommand(Principal.enumAcciones.LOGIN.toString());
+		this.vistaPrincipal.getPanelMenu().getBtnDesconectar().addActionListener(this);
+		this.vistaPrincipal.getPanelMenu().getBtnDesconectar()
+				.setActionCommand(Principal.enumAcciones.DESCONECTAR.toString());
+
+		this.vistaPrincipal.getPanelMenu().getLblFotoAlumno().addMouseListener(this);
+		this.vistaPrincipal.getPanelMenu().getLblFotoReuniones().addMouseListener(this);
+		this.vistaPrincipal.getPanelMenu().getLblFotoHorario().addMouseListener(this);
+		
+		 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Principal.enumAcciones accion = Principal.enumAcciones.valueOf(e.getActionCommand());
+
+		switch (accion) {
+		case LOGIN:
+			incializarServidor();
+			try {
+				dos = new DataOutputStream(cliente.getOutputStream());
+				oos = new ObjectOutputStream(cliente.getOutputStream());
+				dis = new DataInputStream(cliente.getInputStream());
+				ois = new ObjectInputStream(cliente.getInputStream());
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			this.mConfirmarLogin(accion);
+			break;
+		case DESCONECTAR:
+			try {
+				dos.writeInt(4);
+				dos.flush();
+				dis.close();
+				dos.close();
+				this.vistaPrincipal.mVisualizarPaneles(enumAcciones.CARGAR_PANEL_LOGIN);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			break;
+		
+		case VOLVER:
+			this.vistaPrincipal.mVisualizarPaneles(enumAcciones.CARGAR_PANEL_MENU);
+			;
+			break;
+
+		default:
+			break;
+
+		}
+	}
+
+	private void mConfirmarLogin(enumAcciones accion) {
+		// TODO Auto-generated method stub
+
+		try {
+			dos.writeInt(1);
+			dos.flush();
+			dos.writeUTF(this.vistaPrincipal.getPanelLogin().getTextFieldUser().getText());
+			dos.flush();
+			dos.writeUTF(new String(this.vistaPrincipal.getPanelLogin().getTextFieldPass().getPassword()));
+			dos.flush();
+			id = (int) dis.readInt();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (id != 0) {
+			//System.out.println("existo");
+			this.vistaPrincipal.mVisualizarPaneles(enumAcciones.CARGAR_PANEL_MENU);
+		} else {
+			JOptionPane.showMessageDialog(null, "No existe ningun profesor con esas credenciales");
+		}
+	}
+
+	private void incializarServidor() {
+		// TODO Auto-generated method stub
+		try {
+			cliente = new Socket("localhost", 4500);
+			dos = new DataOutputStream(cliente.getOutputStream());
+			dis = new DataInputStream(cliente.getInputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void mouseClicked(MouseEvent e) {
+	
+	}
+
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
